6e6a8af844fb1816027e40c8a3898b248cccf24d
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-03-27 09:48:26 +0000

    [Sandeep].add login register UI

--- FundooWebApi/src/app/app-routing.module.ts
+++ FundooWebApi/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
-
+import { LoginComponent } from './components/login/login.component';
+import {MatInputModule} from '@angular/material/input';
 const routes: Routes = [
-  {path:"registration",component:RegistrationComponent}
+  {path:"registration",component:RegistrationComponent},
+  {path:"login",component:LoginComponent},
+  {path:"",component:LoginComponent}
 ];
 
 @NgModule({
--- FundooWebApi/src/app/app.module.ts
+++ FundooWebApi/src/app/app.module.ts
@@ -9,10 +9,12 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
