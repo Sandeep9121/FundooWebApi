48409f22f4dc3dfb58cea6921c9fee952b5315db
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-03-28 16:09:47 +0000

    [Sandeep].Refactored routing for forgot password

--- FundooWebApi/src/app/app-routing.module.ts
+++ FundooWebApi/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
 const routes: Routes = [
   {path:"registration",component:RegistrationComponent},
   {path:"login",component:LoginComponent},
   {path:"",component:LoginComponent},
-  {path:"resetpassword",component:ResetpasswordComponent}
+  {path:"resetpassword",component:ResetpasswordComponent},
+  {path:"forgotpassword",component:ForgotpasswordComponent}
 ];
 
 @NgModule({
