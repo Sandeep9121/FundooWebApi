d740f982c7ab1947a88b58ddc44aaed1fcd2c3e4
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-03-28 10:17:24 +0000

    [Sandeep].add resetPassword Url

--- FundooWebApi/src/app/app-routing.module.ts
+++ FundooWebApi/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 const routes: Routes = [
   {path:"registration",component:RegistrationComponent},
   {path:"login",component:LoginComponent},
-  {path:"",component:LoginComponent}
+  {path:"",component:LoginComponent},
+  {path:"resetpassword",component:ResetpasswordComponent}
 ];
 
 @NgModule({
