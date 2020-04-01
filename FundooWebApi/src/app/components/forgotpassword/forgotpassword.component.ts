e1fe54d0d5c1843b6055958931ecac6f449ad452
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-04-01 09:26:07 +0100

    [Sandeep].Refactored the forgotpass component.ts

--- FundooWebApi/src/app/components/forgotpassword/forgotpassword.component.ts
+++ FundooWebApi/src/app/components/forgotpassword/forgotpassword.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Forgotpassword } from 'src/app/Model/forgotpassword.model';
 import { UsersService } from 'src/app/Services/users.service';
-import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Validators, FormControl} from '@angular/forms';
 
@@ -11,35 +10,40 @@ import { Validators, FormControl} from '@angular/forms';
   styleUrls: ['./forgotpassword.component.scss']
 })
 export class ForgotpasswordComponent implements OnInit {
-   forgotpassword:Forgotpassword =new Forgotpassword();
-  constructor(private usersService:UsersService,private router:Router,private matSnackBar:MatSnackBar) { }
+   forgotpassword:Forgotpassword =new Forgotpassword(); 
 
-  ngOnInit() {
-  }
-
-  email = new FormControl(null,[Validators.required,Validators.email]);
-
-  getEmailErrorMessage(){
-    return this.email.hasError('required')? "Enter Email Id":
-    this.email.hasError('email')? "EmailId not valid":
-     "";
+   constructor(private userService:UsersService,private matSnackBar:MatSnackBar) { }
+ 
+ 
+ 
+   ngOnInit() {
    }
+ 
+   email = new FormControl(null,[Validators.required,Validators.email]);
+  
+   getEmailErrorMessage(){
+     return this.email.hasError('required')? "Enter Email Id":
+     this.email.hasError('email')? "EmailId not valid":
+      "";
 
-   onSubmit(){
-
-    this.forgotpassword.email = this.email.value;
-     this.usersService.userForgotPassword(this.forgotpassword).subscribe(
-
-      (response:any) => {
-        console.log(response.message);
-        this.matSnackBar.open("Check mail to verify", "succesfull", {duration:5000})
-        this.router.navigate(["/login"]);
-     },
-
-     (error:any)=> {
-       this.matSnackBar.open("Invalid mail", "failed", {duration:5000})
-     }
-     );
-   }
+    }
+    
+       
+    onSubmit(){
+ 
+     this.forgotpassword.email = this.email.value;
+     console.log("-------------------mail",this.email.value);
+      this.userService.userForgotPassword(this.forgotpassword).subscribe(
+ 
+       (response:any) => {
+         console.log(response.message);
+         this.matSnackBar.open("Check mail to verify", "succesfull", {duration:5000})
+      },
+ 
+      (error:any)=> {
+        this.matSnackBar.open("not found user", "failed" , {duration:5000})
+      }
+      );
+    }
 
 }
