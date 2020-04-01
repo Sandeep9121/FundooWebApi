d5894b6856d97652c2b830442bb0a8446aa110db
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-04-01 05:15:05 +0100

    [Sandeep].refactor

--- FundooWebApi/src/app/app-routing.module.ts
+++ FundooWebApi/src/app/app-routing.module.ts
@@ -4,15 +4,21 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { LoginComponent } from './components/login/login.component';
 import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+
+import { UserVerificationComponent } from './components/user-verification/user-verification.component';
+//import { DashboardComponent } from './components/dashboard/dashboard.component';
 const routes: Routes = [
   {path:"registration",component:RegistrationComponent},
   {path:"login",component:LoginComponent},
   {path:"",component:LoginComponent},
-  {path:"resetpassword",component:ResetpasswordComponent},
-  {path:"forgotpassword",component:ForgotpasswordComponent}
+  {path:"resetpassword/:token",component:ResetpasswordComponent},
+  {path:"forgotpassword",component:ForgotpasswordComponent},
+  {path:"userVerification/:token",component:UserVerificationComponent}
+  //,{path:"dashboard",component:DashboardComponent}
 ];
 
-@NgModule({
+@NgModule(
+  {
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
