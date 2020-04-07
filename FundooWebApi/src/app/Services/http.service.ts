e6630be0e1119c8943c5851bdbf2d34286791312
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-04-07 07:04:01 +0100

    [Sandeep].refactored

--- FundooWebApi/src/app/Services/http.service.ts
+++ FundooWebApi/src/app/Services/http.service.ts
@@ -7,10 +7,18 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private http:HttpClient) { }
-  public post(url:any,body:any,head:any){
+
+  public post(url:any , body:any , head:any){
+
     return this.http.post(url,body,head);
   }
-  public put(url:any,body:any,head:any){
+
+  public put(url:any , body:any , head:any){
+
     return this.http.put(url,body,head);
+  }
+
+  public get(url :any,options :any):any{
+    return this.http.get(url,options);
   }
 }
