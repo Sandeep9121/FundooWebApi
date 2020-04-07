4a37e3740b0246a926395917f8e74cd8cbcff3b1
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-04-07 07:05:29 +0100

    [Sandeep].refactored

--- FundooWebApi/src/app/Services/note.service.ts
+++ FundooWebApi/src/app/Services/note.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {environment} from 'src/environments/environment';
-import {HttpHeaders , HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { HttpService } from './http.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +10,27 @@ import { Observable } from 'rxjs';
 export class NoteService {
   private noteApiUrl = environment.noteApiUrl;
   private createNoteUrl = environment.createNoteUrl;
-  
+  private getNotesUrl = environment.getAllNotesUrl;
+
   private httpOptions={
-    headers: new HttpHeaders ({'content-type':'application/json'})
+    headers: new HttpHeaders ({'content-type':'application/json' ,token: localStorage.getItem("token")})
     };
 
   constructor(private httpService:HttpService , private httpClient:HttpClient) { }
-  createNote(noteDetail:any):Observable<any>
+
+  private subject = new Subject<any>();
+  public get autoRefresh() {
+    return this.subject;
+  }
+
+createNote(noteDetail:any):Observable<any>
 {
   console.log("note:",noteDetail);
 return this.httpService.post(this.noteApiUrl+this.createNoteUrl,noteDetail,{headers:new HttpHeaders({'token':localStorage.token})});
 }
 
+getAllNotes():Observable<any>{
+
+  return this.httpService.get(this.noteApiUrl+this.getNotesUrl,this.httpOptions);
+}
 }
