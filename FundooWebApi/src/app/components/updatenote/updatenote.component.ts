c0176c3b23a3fa58646062229c718a98ea39d3e3
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-04-13 11:51:27 +0100

    [Sandeep].Refactored

---
+++ FundooWebApi/src/app/components/updatenote/updatenote.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Note } from 'src/app/Model/note.model';
+import { NoteService } from 'src/app/Services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef , MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-updatenote',
+  templateUrl: './updatenote.component.html',
+  styleUrls: ['./updatenote.component.scss']
+})
+export class UpdatenoteComponent implements OnInit {
+  note :Note;
+  constructor(private noteService: NoteService,public matDialogRef: MatDialogRef<UpdatenoteComponent>,
+    private snackbar: MatSnackBar , @Inject(MAT_DIALOG_DATA) public data: any) { 
+      this.note=this.data.note;
+    }
+    // this.note=this.data.note;
+
+  ngOnInit() {
+  }
+  
+  onSubmit(){
+    this.matDialogRef.close();
+    this.noteService.updateNote(this.note.notesId , this.note).subscribe(
+      (response: any) => {
+        this.snackbar.open(response['message'], "ok", {duration: 4000});
+      },
+      errors => {
+        this.snackbar.open(errors.error.message, "failed", {duration: 4000 });
+      }
+    );
+  }
+
+
+
+}
